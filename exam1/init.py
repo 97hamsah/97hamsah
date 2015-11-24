3372296ddca00bc055898837f231059f1cfda14a
97hamsah <hampusssahlin@gmail.com>
2015-11-24 09:34:12 +0100

    Swag
    Jag har lagt till knappaj och grejer

--- exam1/init.py
+++ exam1/init.py
@@ -17,18 +17,66 @@ class Exam1(QMainWindow):
         self.setWindowTitle("Exam1 template")
         
         self.initUI()
+        
     
     def initUI(self):
         """This method is called when the application first runs
             The method initializes all the gui """
         self.frame = QWidget(self)
         self.setCentralWidget(self.frame)
+        self.layout = QVBoxLayout
+        self.form_layout = QFormLayout
+        self.resize(200,500)
+        
+        self.name = QLineEdit(self)
+        #Lägger till en textruta
+        self.name.move(50,0)
+        #Flyttar textrutan
+        self.text = QLabel("Färg:", self)
+        self.text.move(25,0)
+        
+        
+        
+        self.name2 = QLineEdit(self)
+        #Lägger till en textruta
+        self.name2.move(50,30)
+        #Flyttar textrutan
+        self.text2 = QLabel("Kön:", self)
+        self.text2.move(25,30)
+        
+        
+        
+        self.djur = QComboBox(self)
+        #skapar en "Combobox"
+        self.djur.addItem("Välj djur:")
+        self.djur.addItem("Katt")
+        self.djur.addItem("Fågel")
+        #Lägger till olika alternativ i comboboxen
+        self.djur.move(50,70)
+        #Flyttar komboboxen
+        
+        self.knapp_kor = QPushButton("Kör",self)
+        #Skapar en knapp som det står "kör" på
+        self.knapp_kor.move(50,100)
+        #Flyttar knappen
+        
+        
+        self.knapp_rensa = QPushButton("Rensa", self)
+        self.knapp_rensa.clicked.connect(self.rensa)
+        self.knapp_rensa.move(50,130)
+        
+        
+    def rensa(self):
+        self.name.setText("")
+        self.name2.setText("")
+        self.djur.setText("Välj djur:")
+        
         
     def run(self):
         self.show()
         sys.exit(app.exec_())
 
-MyProgram().run()
+
 app = QApplication(sys.argv)
 
 # Start the program by calling the run method. 
